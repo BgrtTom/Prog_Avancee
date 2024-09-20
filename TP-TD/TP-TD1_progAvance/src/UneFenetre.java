eabd13b767cf344c1f34327ddbe5252116dfbaf1
Tom Bogaert <bogaert.tom92@gmail.com>
2024-09-20 16:39:05 +0200

    SRC : Ajout du code permettant l'affichage de la fenêtre

--- TP-TD/TP-TD1_progAvance/src/UneFenetre.java
+++ TP-TD/TP-TD1_progAvance/src/UneFenetre.java
@@ -3,11 +3,22 @@ import javax.swing.*;
 
 class UneFenetre extends JFrame 
 {
-    UnMobile sonMobile;
-    private final int LARG=400, HAUT=250;
-    
+    UnMobile TacheMobile;
+    private final int LARG=800, HAUT=500;
+
     public UneFenetre()
     {
+        JFrame frame = new JFrame("Une Fenetre");
+        frame.setSize(LARG, HAUT);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        TacheMobile = new UnMobile(LARG,HAUT);
+        frame.add(TacheMobile);
+        Thread SupportTache = new Thread (TacheMobile);
+
+        SupportTache.start();
 	// TODO 
 	// ajouter sonMobile a la fenetre
 	// creer une thread laThread avec sonMobile

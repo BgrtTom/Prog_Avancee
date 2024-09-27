ed86669f0e0fc01bb94ac3fd29bccb1cef9d5723
Tom Bogaert <bogaert.tom92@gmail.com>
2024-09-27 14:38:52 +0200

    SRC : Modification de la méthode run afin que le mobile fasse demi-tour

--- TP-TD/TP-TD1_progAvance/src/UnMobile.java
+++ TP-TD/TP-TD1_progAvance/src/UnMobile.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 class UnMobile extends JPanel implements Runnable
 {
     int saLargeur, saHauteur, sonDebDessin;
-    final int sonPas = 10, sonTemps=50, sonCote=40;
+    final int sonPas = 10, sonTemps=25, sonCote=40;
     
     UnMobile(int telleLargeur, int telleHauteur)
     {
@@ -16,14 +16,22 @@ class UnMobile extends JPanel implements Runnable
 
     public void run()
     {
-	for (sonDebDessin=0; sonDebDessin < saLargeur - sonPas; sonDebDessin+= sonPas)
+	for (sonDebDessin=0; sonDebDessin < saLargeur - 8*sonPas ; sonDebDessin+= sonPas)
 	    {
 		repaint();
 		try{Thread.sleep(sonTemps);}
 		catch (InterruptedException telleExcp)
 		    {telleExcp.printStackTrace();}
 	    }
-    }
+
+	for (sonDebDessin = saLargeur - sonCote; sonDebDessin > 0; sonDebDessin-= sonPas)
+	{
+		repaint();
+		try{Thread.sleep(sonTemps);}
+		catch (InterruptedException telleExcp)
+		{telleExcp.printStackTrace();}
+	}
+	}
 
     public void paintComponent(Graphics telCG)
     {

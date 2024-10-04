7eea8d228c83826fcd9108562b59430ecd345994
Tom Bogaert <bogaert.tom92@gmail.com>
2024-10-04 14:34:32 +0200

    src(TP1): Modification du code des classes afin d'afficher plusieurs Mobiles possédant des vitesses différentes

--- TP-TD/TP-TD1_progAvance/src/UnMobile.java
+++ TP-TD/TP-TD1_progAvance/src/UnMobile.java
@@ -1,41 +1,68 @@
 import java.awt.*;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 import javax.swing.*;
 
 class UnMobile extends JPanel implements Runnable
 {
     int saLargeur, saHauteur, sonDebDessin;
-    final int sonPas = 10, sonTemps=25, sonCote=40;
+    final int sonPas = 10, sonCote=40;
+	int sonTemps;
+	boolean paused = false;
     
     UnMobile(int telleLargeur, int telleHauteur)
     {
 	super();
 	saLargeur = telleLargeur;
 	saHauteur = telleHauteur;
+	sonTemps = sonTemps;
 	setSize(telleLargeur, telleHauteur);
     }
 
     public void run()
     {
+	Thread th = new Thread(this);
 	for (sonDebDessin=0; sonDebDessin < saLargeur - 8*sonPas ; sonDebDessin+= sonPas)
 	    {
-		repaint();
-		try{Thread.sleep(sonTemps);}
+
+		if (paused){
+			th.suspend();
+		}
+			repaint();
+		try{
+			Thread.sleep((int) (Math.random()*75));
+		}
 		catch (InterruptedException telleExcp)
 		    {telleExcp.printStackTrace();}
 	    }
 
 	for (sonDebDessin = saLargeur - sonCote; sonDebDessin > 0; sonDebDessin-= sonPas)
 	{
+		if (paused){
+			th.suspend();
+		}
 		repaint();
-		try{Thread.sleep(sonTemps);}
+		try{Thread.sleep((int) (Math.random()*75));}
 		catch (InterruptedException telleExcp)
 		{telleExcp.printStackTrace();}
 	}
 	}
+
 
     public void paintComponent(Graphics telCG)
     {
 	super.paintComponent(telCG);
 	telCG.fillRect(sonDebDessin, saHauteur/2, sonCote, sonCote);
     }
+
+	public void suspend()
+	{
+		paused = true;
+	}
+	public void resume()
+	{
+		paused = false;
+	}
+
+
 }
--- TP-TD/TP-TD1_progAvance/src/UneFenetre.java
+++ TP-TD/TP-TD1_progAvance/src/UneFenetre.java
@@ -1,24 +1,56 @@
 import java.awt.*;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 class UneFenetre extends JFrame 
 {
     UnMobile TacheMobile;
-    private final int LARG=800, HAUT=500;
+    private final int LARG=1000, HAUT=700;
 
-    public UneFenetre()
+    public UneFenetre(int nbMobile)
     {
+
+        Container leConteneur = getContentPane();
+        leConteneur.setLayout (new GridLayout(nbMobile, 1));
+
         JFrame frame = new JFrame("Une Fenetre");
         frame.setSize(LARG, HAUT);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        for (int i = 0; i < nbMobile; i++) {
+            TacheMobile = new UnMobile(LARG,HAUT/nbMobile);
+            leConteneur.add(TacheMobile);
+
+            Thread SupportTache = new Thread (TacheMobile);
+            SupportTache.start();
+        }
+
+
+        //JButton button= new JButton ("Start/Stop");
+        //frame.add(button, BorderLayout.WEST);
+        frame.add(leConteneur);
+
+
+
+
+
+        /*
+        button.addActionListener(e -> {
+            if(button.getText().equals("Start/Stop")) {
+                button.setText("Start/Stop");
+                TacheMobile.suspend();
+            } else {
+                button.setText("Start/Stop");
+                TacheMobile.resume();
+            }
+        });*/
+
         frame.setVisible(true);
 
-        TacheMobile = new UnMobile(LARG,HAUT);
-        frame.add(TacheMobile);
-        Thread SupportTache = new Thread (TacheMobile);
 
-        SupportTache.start();
+
 	// TODO 
 	// ajouter sonMobile a la fenetre
 	// creer une thread laThread avec sonMobile
